dbf9b33de66eaac5a448a06bb20624bdd597d89d
Dave South <dave@souths.org>
2022-05-06 14:41:03 -0600

    Fixed sentry.io configuration (I hope)
    Close #9

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,23 +1,18 @@
 Sentry.init do |config|
 
-  # Sentry is annoying in development mode
-  if Rails.env.production?
-    config.dsn = Rails.application.credentials.sentry_dsn
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  # Configure application credentials on sentry.io
+  config.dsn = Rails.application.credentials.sentry_dsn
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    # Filter out confidential parameters
-    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-    config.before_send = lambda do |event, hint|
-      filter.filter(event.to_hash)
-    end
+  # Only in production (it's annoying in development)
+  config.enabled_environments = %w[production]
 
-    # Set tracesSampleRate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production
-    # config.traces_sample_rate = 1.0
-    # or
-    # config.traces_sampler = lambda do |context|
-      # true
-    # end
-  end
+  # Set tracesSampleRate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production
+  # config.traces_sample_rate = 1.0
+  # or
+  # config.traces_sampler = lambda do |context|
+    # true
+  # end
 end
