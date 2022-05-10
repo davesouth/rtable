54777d9cd219218e7156aa4dc075977239e4f0af
Dave South <dave@souths.org>
2022-05-10 16:18:14 -0600

    Improve account/categories CRUD
    Close #18

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,12 @@
 class CategoriesController < AccountableController
+  # @account set in accountable controller
+
+  # Set @category
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = @account.categories.all
   end
 
   # GET /categories/1 or /categories/1.json
@@ -21,11 +24,11 @@ class CategoriesController < AccountableController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = @account.categories.build(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+        format.html { redirect_to [@account, @category], notice: "Category was successfully created." }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +41,7 @@ class CategoriesController < AccountableController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
+        format.html { redirect_to [@account, @category], notice: "Category was successfully updated." }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +55,7 @@ class CategoriesController < AccountableController
     @category.destroy
 
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
+      format.html { redirect_to [@account, :categories], notice: "Category was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -60,11 +63,11 @@ class CategoriesController < AccountableController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = @account.categories.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:account_id, :name, :slug)
+      params.require(:category).permit(:name, :slug)
     end
 end
