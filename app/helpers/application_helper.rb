1aa03a80ea39569e9822e729d37b70de94ef9650
Dave South <dave@souths.org>
2022-05-19 15:17:00 -0600

    Extracted form errors into helper
    Close #56

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
+  def display_form_errors(obj)
+    if obj.errors.any?
+      haml_tag '#error_explanation' do
+        haml_tag :h2, "#{pluralize(obj.errors.count, 'error')} reported"
+        haml_tag :ul do
+          obj.errors.full_messages.each do |msg|
+            haml_tag :li, msg
+          end
+        end
+      end
+    end
+  end
+
 end
