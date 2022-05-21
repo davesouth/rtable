a220d4f5b6be580549da46e331099b86cf068931
Dave South <dave@souths.org>
2022-05-21 09:03:24 -0600

    Another fix to current_user failing with missing session.user_id
    Close #60

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -10,24 +10,19 @@ class AuthorizationController < DomainableController
   # Set current user card
   def current_user
     @current_user ||= Card.find(session[:user_id]) if session[:user_id]
-  rescue Mongoid::Errors::DocumentNotFound
-    save_path_and_redirect_to_login
   end ; helper_method :current_user
 
   # Set current user or forward to login
   def set_current_user
     User.current = current_user
     yield
-  rescue CanCan::AccessDenied
-    save_path_and_redirect_to_login
+  rescue
+    # Save current page for redirect after login
+    session['redirect_path'] = url_for(request.params)
+    # Redirect to login page
+    redirect_to new_session_path
   ensure
     User.current = nil
   end ; private :set_current_user
 
-  def save_path_and_redirect_to_login
-    # Save current page for redirect after login
-    session['redirect_path'] = url_for(request.params)
-    # Redirect to login page
-    redirect_to(new_session_path) and return
-  end
 end
