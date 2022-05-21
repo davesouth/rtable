4695af7391c7b41e364a08d9545c55a3b19b23b2
Dave South <dave@souths.org>
2022-05-21 08:54:28 -0600

    Fixed current_user when session.user_id not found
    Close #60

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -10,6 +10,8 @@ class AuthorizationController < DomainableController
   # Set current user card
   def current_user
     @current_user ||= Card.find(session[:user_id]) if session[:user_id]
+  rescue Mongoid::Errors::DocumentNotFound
+    save_path_and_redirect_to_login
   end ; helper_method :current_user
 
   # Set current user or forward to login
@@ -17,12 +19,15 @@ class AuthorizationController < DomainableController
     User.current = current_user
     yield
   rescue CanCan::AccessDenied
-    # Save current page for redirect after login
-    session['redirect_path'] = url_for(request.params)
-    # Redirect to login page
-    redirect_to new_session_path
+    save_path_and_redirect_to_login
   ensure
     User.current = nil
   end ; private :set_current_user
 
+  def save_path_and_redirect_to_login
+    # Save current page for redirect after login
+    session['redirect_path'] = url_for(request.params)
+    # Redirect to login page
+    redirect_to new_session_path
+  end
 end
