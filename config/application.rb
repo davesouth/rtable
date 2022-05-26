4babd1017c5c60ba0ba40998c3d77d14486b0fdb
Dave South <dave@souths.org>
2022-05-26 16:02:17 -0600

    Set application timezone to UTC

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,10 @@ module Rtable
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # We want to store everything as UTC
+    config.time_zone = 'UTC'
+
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
