c62112ba924a138f7ae3a21345fca6aec1e7943a
Dave South <dave@souths.org>
2022-10-17 16:28:51 -0600

    Gem: Upgrade to HAML 6.0.7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,15 @@ module ApplicationHelper
 
   def display_form_errors(obj)
     if obj.errors.any?
-      haml_tag '#error_explanation' do
-        haml_tag :h2, "#{pluralize(obj.errors.count, 'error')} reported"
-        haml_tag :ul do
-          obj.errors.full_messages.each do |msg|
-            haml_tag :li, msg
+      content_tag :div, id: 'error_explanation' do
+        concat(content_tag :h2, "#{pluralize(obj.errors.count, 'error')} reported")
+        concat(
+          content_tag(:ul) do
+            obj.errors.full_messages.each do |msg|
+              concat(content_tag :li, msg)
+            end
           end
-        end
+        )
       end
     end
   end
