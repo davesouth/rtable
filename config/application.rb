679327c3871900aee1b5d98c08dd72574b831ab5
Dave South <dave@monolithicdome.com>
2024-10-17 19:13:36 -0600

    Gem: Rails 7.2.1.1 upgrade

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,12 @@ Bundler.require(*Rails.groups)
 module Rtable
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -35,10 +35,6 @@ module Rtable
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # We want to store everything as UTC
-    config.time_zone = 'UTC'
-
 
     # Don't generate system test files.
     config.generators.system_tests = nil
